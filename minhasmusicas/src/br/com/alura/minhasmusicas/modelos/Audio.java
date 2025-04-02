a77a79262d4dd545b6567e298a15a710a83fe7bc
Emílio Silva <emiliosarges@gmail.com>
2025-04-02 20:44:45 -0300

    Desafio Finalizado

--- minhasmusicas/src/br/com/alura/minhasmusicas/modelos/Audio.java
+++ minhasmusicas/src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -4,7 +4,33 @@ public class Audio {
     private String titulo;
     private int totalReproducoes;
     private int totalCurtidas;
-    private double classificacao;
+    private int classificacao;
 
-    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte() {
+        this.totalCurtidas++;
+    }
+
+    public void reproduz() {
+        this.totalReproducoes++;
+    }
 }
